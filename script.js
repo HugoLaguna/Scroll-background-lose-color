4536f2ac12a07402f53a14e7a513d26305fb6f20
Hugo  Laguna <hugolaguna@iMac-de-Hugo.local>
2018-05-27 23:51:56 -0500

    firts relase

---
+++ script.js
@@ -0,0 +1,39 @@
+
+var counter = 0;
+
+function reClassBody(c){
+    if(document.body.classList.contains(c)){
+        document.body.classList.remove(c);
+    }else{
+        document.body.classList.add(c);
+    }
+}
+
+function changeBlendBody(Effect){
+    document.body.style.backgroundBlendMode = Effect;
+}
+function changeColorBody(Color){
+    document.body.style.backgroundColor = Color;
+}
+
+
+
+function BackgroundColorEffect(i){
+    counter = counter + i;
+    if (counter >= 100){
+        counter = 1;
+        reClassBody('colorless');
+    }else if(counter <= 0){
+        reClassBody('colorless');
+    }
+    document.getElementById('line').style.height=counter+'%';
+}
+
+// Listener Scroll 
+document.addEventListener("DOMMouseScroll", function(event) {
+    BackgroundColorEffect(event.detail);
+});
+
+document.addEventListener("scroll", function(event) {
+    BackgroundColorEffect(event.detail);
+});
